60c44443abf9ec5b2f31ca50b913ae7de128018b
Juju <kosyuska@yandex.ru>
2022-01-29 21:41:05 +0300

    feat: add Task4

---
+++ Task4/DocumentWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class DocumentWorker
+    {
+        public void OpenDocument()
+        { }
+        public void EditDocument()
+        { }
+        public void SaveDocument()
+        { }
+    }
+}
