2a75c200241abf67cb532adfebcc2f8d22c8eae5
Juju <kosyuska@yandex.ru>
2022-01-29 22:43:56 +0300

    feat: finish Task4

--- Task4/DocumentWorker.cs
+++ Task4/DocumentWorker.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task4
+﻿namespace Task4
 {
     internal class DocumentWorker
     {
         public void OpenDocument()
-        { }
-        public void EditDocument()
-        { }
-        public void SaveDocument()
-        { }
+        {
+            Console.WriteLine("Документ  открыт");
+        }
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редактирование  документа  доступно  в  версии  Про");
+        }
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Сохранение  документа  доступно  в  версии Про");
+        }
+
+        public void Show()
+        {
+            this.OpenDocument();
+            this.EditDocument();
+            this.SaveDocument();
+        }
     }
 }
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Task4;
+
+Console.WriteLine("Hello, Teacher!");
+
+const string keyProDocument = "qwertyPro";
+const string keyExpertDocument = "qwertyExp";
+
+while (true)
+{
+    Console.WriteLine("\nВведите ключ доступа. В конце ключа через пробел укажите версию программы: pro - ProDocumentWorker, exp - ExpertDocumentWorker");
+
+    string? key = Console.ReadLine();
+
+    if (!String.IsNullOrEmpty(key))
+    {
+        string[] keySpaceArray = key.Split(' ');
+        if (keySpaceArray.Length != 2)
+        {
+            Console.WriteLine("Вы ввели не в правильном формате: ключ версия (через пробел)");
+            continue;
+        }
+        else
+        {
+            switch (keySpaceArray[1])
+            {
+                case "pro":
+                    bool keyCheckPro = keySpaceArray[0] == keyProDocument;
+                    if (keyCheckPro)
+                    {
+                        Console.WriteLine("Вы ввели ключ от версии ProDocumentWorker");
+                        DocumentWorker proDocWorker = new ProDocumentWorker();
+                        proDocWorker.Show();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели неправильный ключ от версии ProDocument");
+                    }
+                    break;
+                case "exp":
+                    bool keyCheckExp = keySpaceArray[0] == keyExpertDocument;
+                    if (keyCheckExp)
+                    {
+                        Console.WriteLine("Вы ввели ключ от версии ExpertDocumentWorker");
+                        DocumentWorker expDocWorker = new ExpertDocumentWorker();
+                        expDocWorker.Show();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели неправильный ключ от версии ExpertDocument");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная версия программы");
+                    break;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ничего не ввели. Можете использовать только бесплантную версию");
+        DocumentWorker docWorker = new DocumentWorker();
+        docWorker.Show();
+    }
+}
